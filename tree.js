06fb68c0354deb58dfd158c0e26b2f966db42e30
RITVIKTOTAM <74972856+RITVIKTOTAM@users.noreply.github.com>
2021-01-08 15:35:22 +0530

    Add files via upload

---
+++ tree.js
@@ -0,0 +1,29 @@
+class Tree{
+    constructor(x, y, width,height) {
+     
+        var options = {
+          'isStatic':true,
+          'restitution':0.3,
+          'friction':0.5,
+          'density':1.2
+      }
+      
+      
+      this.body = Bodies.rectangle(x, y, width,height, options);
+      this.width = width;
+      this.height = height;
+      this.image = loadImage("tree.png");
+      World.add(world,this.body);
+      
+    }
+    display(){
+      var pos =this.body.position;
+      push();
+      translate(pos.x, pos.y);
+      fill(255);
+      imageMode(CENTER);
+      image(this.image,0, 0, this.width,this.height);
+      pop();
+    }
+  };
+  
